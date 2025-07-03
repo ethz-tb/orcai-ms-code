e442de02dbecfadac8ef4600376bf29d150e21c5
Daniel Angst <daniel.angst@env.ethz.ch>
2025-07-03 07:54:22 +0200

    add hyperparameter search table

--- .justfile
+++ .justfile
@@ -9,6 +9,7 @@ make-plots: extract-metrics
 make-tables: extract-metrics 
     Rscript analysis_scripts/confusion_table.R
     Rscript analysis_scripts/misclassification_table.R
+    Rscript analysis_scripts/hps_trials_table.R
 
 make-stats: extract-metrics
     Rscript analysis_scripts/training_time.R
@@ -17,12 +18,18 @@ make-stats: extract-metrics
 make-pdf-tables: make-tables
     pdflatex -output-directory analysis_scripts/output analysis_scripts/table4_confusion.tex
     rm analysis_scripts/output/table4_confusion.aux analysis_scripts/output/table4_confusion.log
+    
     pdflatex -output-directory analysis_scripts/output analysis_scripts/table5_misclassification_combined.tex
     rm analysis_scripts/output/table5_misclassification_combined.aux analysis_scripts/output/table5_misclassification_combined.log
+    
     pdflatex -output-directory analysis_scripts/output analysis_scripts/table5_misclassification_TP.tex
     rm  analysis_scripts/output/table5_misclassification_TP.aux analysis_scripts/output/table5_misclassification_TP.log
+    
     pdflatex -output-directory analysis_scripts/output analysis_scripts/table6_misclassification_PT.tex
     rm analysis_scripts/output/table6_misclassification_PT.aux analysis_scripts/output/table6_misclassification_PT.log
+    
+    pdflatex -output-directory analysis_scripts/output analysis_scripts/tableSI1_hps_trials.tex
+    rm analysis_scripts/output/tableSI1_hps_trials.aux analysis_scripts/output/tableSI1_hps_trials.log
 
 extract-metrics:
     echo "Extracting metrics..."
---
+++ analysis_scripts/hps_trials_table.R
@@ -0,0 +1,60 @@
+library(tidyverse)
+library(here)
+library(knitr)
+library(kableExtra)
+
+hps_trials_raw <- read_csv(
+    here("hyperparameter_search", "orcai-v1-3750-LSTM_HPS", "hps_logs", "all_trials.csv"),
+    col_types = cols(
+        filters = col_character(),
+        kernel_size = col_double(),
+        dropout_rate = col_double(),
+        batch_size = col_double(),
+        lstm_units = col_double(),
+        `tuner/epochs` = col_double(),
+        `tuner/initial_epoch` = col_double(),
+        `tuner/bracket` = col_double(),
+        `tuner/round` = col_double(),
+        score = col_double(),
+        status = col_character(),
+        MBA = col_double(),
+        loss = col_double(),
+        val_MBA = col_double(),
+        val_loss = col_double(),
+        `tuner/trial_id` = col_character()
+    )
+)
+
+hps_trials <- hps_trials_raw |>
+    mutate(
+        filters = case_match(
+            filters,
+            "set1" ~ "[10, 20, 30, 40]",
+            "set2" ~ "[20, 30, 40, 50]",
+            "set3" ~ "[30, 40, 50, 60]"
+        )
+    ) |>
+    rename_with(~ str_replace(.x, "tuner/", "")) |>
+    rename_with(~ str_replace(.x, "_", " ")) |>
+    select(
+        filters:`lstm units`,
+        epochs,
+        MBA, loss,
+        `val MBA`, `val loss`
+    )
+
+colnames(hps_trials)
+
+hps_trials |>
+    arrange(desc(`val MBA`)) |>
+    kbl(
+        format = "latex", digits = 4,
+        col.names = c(
+            "filters", "kernel size", "dropout rate", "batch size", "lstm units",
+            "epochs", "MBA", "loss", "MBA", "loss"
+        ),
+        booktabs = TRUE
+    ) |>
+    add_header_above(c("hyperparameter" = 5, " " = 1, "test data" = 2, "validation data" = 2)) |>
+    row_spec(1, bold = TRUE) |>
+    write_lines(file = here("analysis_scripts", "output", "hps_trials_table.tex"))
